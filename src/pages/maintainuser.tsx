fde1fea6a11191ea231a7bed9044bd55e688d613
Premkumar1617 <premkumar.malyala12@gmail.com>
2026-03-05 08:56:35 +0530

    Comment out unused imports in maintainuser.tsx
    Comment out unused imports for clarity.

--- src/pages/maintainuser.tsx
+++ src/pages/maintainuser.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { type User } from "../types/user";
+//import { useEffect, useState } from "react";
+//import { type User } from "../types/user";
 
 
 const MaintainUser =()=>{
