57176ae1ca8d99792f234476033cf2dc9498a24d
Premkumar1617 <Premkumar.malyala12@gmail.com>
2026-03-05 08:48:38 +0530

    commented unused methodes

--- src/pages/maintainuser.tsx
+++ src/pages/maintainuser.tsx
@@ -3,9 +3,9 @@ import { type User } from "../types/user";
 
 
 const MaintainUser =()=>{
-   const [user,setUsers]=useState<User[]>([]);
-   const [loading,setLoading]=useState(true);
-   const [error,setError]=useState<string | null>(null);
+  //  const [user,setUsers]=useState<User[]>([]);
+  //  const [loading,setLoading]=useState(true);
+  //  const [error,setError]=useState<string | null>(null);
 
    useEffect(()=>{
      try{
